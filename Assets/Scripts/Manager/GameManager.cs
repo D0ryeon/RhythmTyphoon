077f9ea0490c7fcd6c04a0922ea58a06df260912
KimDoHyeon <hj2471755@naver.com>
2023-10-16 19:56:18 +0900

    GameManager로 prefabs 생성 작성 중

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameManager : Singletone<GameManager>
 {
+    public bool isRhythmMode = false;
+
+    private GameObject playerWalk;
+    private GameObject playerAttack;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerWalk = Resources.Load<GameObject>("Prefabs/PlayerWalk");
+        playerAttack = Resources.Load<GameObject>("Prefabs/PlayerAttack");
+        //InstantiateGameObject(playerWalk, new Vector2(5, 0));
+        //InstantiateGameObject(playerAttack, new Vector2(-5, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void InstantiateGameObject(GameObject gameObject, Vector2 vector2)
+    {
+        Instantiate(gameObject, vector2, Quaternion.identity);
     }
 }
