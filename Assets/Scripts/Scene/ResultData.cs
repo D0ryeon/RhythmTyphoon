9bf839ca775792339010dcb4072038b426aba2ea
uYulmu <141593562+uYulmu@users.noreply.github.com>
2023-10-19 17:03:15 +0900

    Result

--- Assets/Scripts/Scene/ResultData.cs
+++ Assets/Scripts/Scene/ResultData.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ResultData : MonoBehaviour
 {
-    public int count;
+    public GameObject PlayData;
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI ComboText;
 
     // Start is called before the first frame update
     void Start()
     {
+        int Score = PlayData.GetComponent<UIManager>().currentScore;
+        int Combo = PlayData.GetComponent<UIManager>().combo;
 
+        ScoreText.text = Score.ToString();
+        ComboText.text = Combo.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
